b4c01eab06d0a54a697caed2853d68dfbba80449
chepe11295 <josemarroquin_10@hotmail.com>
2016-02-18 23:51:05 -0600

    my first hello world

---
+++ my first project Hello World Jose Marroquin/src/my.first.project.Hello.World.Jose.Marroquin.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : my.cpp
+// Author      : Jose Marroquin
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "¡¡¡hello world!!!" << endl; // prints ¡¡¡hello world!!!
+	return 0;
+}
